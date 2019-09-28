11565657eac32261afd8a100cb7152cf8c5f941d
성민 한 <kbg01011@gmail.com>
2019-09-28 15:15:48 +0900

    player/bullet

---
+++ MiniProject/Assets/01.Script/Object/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : BaseObject
+{
+    const int iMapSizeExample = 10;
+    const int iBulletSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iSpeed = iBulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletUpdate();
+    }
+
+    void BulletUpdate()
+    {
+        gameObject.transform.position += new Vector3(Time.deltaTime * iSpeed, 0, 0);
+        Vector3 pos = transform.position;
+
+        if (pos.x > iMapSizeExample || pos.x < -iMapSizeExample ||
+            pos.y > iMapSizeExample || pos.y < -iMapSizeExample)
+            gameObject.SetActive(false);
+    }
+}
---
+++ MiniProject/Assets/01.Script/Object/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : BaseObject
+{
+    List<GameObject> objBullets = new List<GameObject>();
+
+    public GameObject objBullet;
+
+    private const int iBulletCnt = 20;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        iSpeed = 10;
+        for (int i = 0; i < iBulletCnt; ++i)
+        {
+            GameObject bullet = Instantiate(objBullet);
+            bullet.SetActive(false);
+            objBullets.Add(bullet);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        fHorizontal = Input.GetAxis("Horizontal");
+        fVertical = Input.GetAxis("Vertical");
+
+        Debug.Log("(" + fHorizontal + " : " + fVertical + ")");
+
+        Vector3 vMovement = new Vector3(fHorizontal, fVertical, 0);
+        transform.position += vMovement * iSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown("z"))
+        {
+            BulletSetActivate();
+        }
+    }
+
+    void BulletSetActivate()
+    {
+        for (int i = 0; i < iBulletCnt; ++i)
+        {
+            if(!objBullets[i].activeSelf)
+            {
+                objBullets[i].SetActive(true);
+                objBullets[i].GetComponent<BaseObject>().SetObjectStartPos(transform.position);
+                objBullets[i].GetComponent<BaseObject>().fHorizontal = fHorizontal;
+                objBullets[i].GetComponent<BaseObject>().fVertical = fVertical;
+                break;
+            }
+        }
+    }
+}
