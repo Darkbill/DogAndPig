065d12eca88610a87e57252eac06a86432e00af9
crusader0517 <crusader0517@naver.com>
2019-09-28 11:25:50 +0900

    초기구성2

---
+++ MiniProject/Assets/01.Script/Manager/GameMng.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMng : MonoBehaviour
+{
+	#region SINGLETON
+	static GameMng _instance = null;
+
+	public static GameMng Ins
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType(typeof(GameMng)) as GameMng;
+				if (_instance == null)
+				{
+					_instance = new GameObject("GameMng", typeof(GameMng)).GetComponent<GameMng>();
+				}
+			}
+
+			return _instance;
+		}
+	}
+	#endregion
+
+}
---
+++ MiniProject/Assets/01.Script/Manager/UIMng.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UIMng : MonoBehaviour
+{
+	#region SINGLETON
+	static UIMng _instance = null;
+
+	public static UIMng Ins
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType(typeof(UIMng)) as UIMng;
+				if (_instance == null)
+				{
+					_instance = new GameObject("UIMng", typeof(UIMng)).GetComponent<UIMng>();
+				}
+			}
+
+			return _instance;
+		}
+	}
+	#endregion
+}
