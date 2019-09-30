c57ddc9111c378dcf9bf949651eea68a3ad1c3c4
crusader0517 <crusader0517@naver.com>
2019-09-30 22:33:48 +0900

    sum

---
+++ MiniProject/Assets/01.Script/Data/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : TableBase
+{
+	public int itemID;
+	public string itemName;
+	public ItemData(int i, string n)
+	{
+		itemID = i;
+		itemName = n;
+	}
+}
--- MiniProject/Assets/01.Script/Player/Player.cs
+++ MiniProject/Assets/01.Script/Player/Player.cs
@@ -11,36 +11,47 @@ public class Player : MonoBehaviour
 	// * public * //
 	public float fHorizontal { get; set; }
 	public float fVertical { get; set; }
+	public bool isMove;
 	public Movement Mov;
 
 	// * private * //
 	private const int iBulletCnt = 20;
 	protected int iSpeed;
 
-    private void Awake()
-    {
+	private void Awake()
+	{
 		PlayerSetting();
-    }
+	}
 	private void PlayerSetting()
 	{
 		iSpeed = 2;
 	}
 	private void Update()
 	{
-		PlayerMove();
+		if (isMove)
+		{
+			MoveToJoyStick();
+		}
+		else PlayerMove();
 	}
 
 
-    void PlayerMove()
-    {
-        fHorizontal = Input.GetAxis("Horizontal");
-        fVertical = Input.GetAxis("Vertical");
+	void PlayerMove()
+	{
+		fHorizontal = Input.GetAxis("Horizontal");
+		fVertical = Input.GetAxis("Vertical");
 
-        Mov.iSpeed = iSpeed;
-        transform.position += Mov.Move(fHorizontal, fVertical);
-    }
+		Mov.iSpeed = iSpeed;
+		transform.position += Mov.Move(fHorizontal, fVertical);
+	}
+	public void MoveToJoyStick()
+	{
+		Vector3 direction = UIMngInGame.Ins.GetJoyStickDirection();
+		gameObject.transform.position += new Vector3(direction.x, direction.y, 0) * iSpeed * Time.deltaTime;
+	}
+	//TODO : 벽 부딪히면 넘어가지않게
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		
+
 	}
 }
