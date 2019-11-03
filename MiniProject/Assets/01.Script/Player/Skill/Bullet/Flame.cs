d8d9a1cc6e00e50713b822fd5684836350a71673
hychan0517 <53807283+hychan0517@users.noreply.github.com>
2019-11-03 19:08:21 +0900

    flame 충동체크

--- MiniProject/Assets/01.Script/Player/Skill/Bullet/Flame.cs
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/Flame.cs
@@ -4,13 +4,26 @@ public class Flame : BulletPlayerSkill
 {
 	private float damage;
 	private eAttackType attackType;
+	public BoxCollider2D frameCollider;
+	private float attackTime = 0.1f;
+	private float cTime = 0;
 	public void Setting(eAttackType type,float _damage)
 	{
 		damage = _damage;
 		attackType = type;
 	}
+	private void OnDisable()
+	{
+		cTime = 0;
+	}
 	private void Update()
 	{
+		cTime += Time.deltaTime;
+		if(cTime >= attackTime)
+		{
+			frameCollider.enabled = !frameCollider.enabled;
+			cTime = 0;
+		}
 		gameObject.transform.position = GameMng.Ins.player.transform.position + GameMng.Ins.player.GetForward() * 2f;
 		gameObject.transform.eulerAngles = new Vector3(0, 0, GameMng.Ins.player.degree);
 	}
