d15f48c00b0b03e3be3ea6a2dce38f726ada8544
hychan0517 <53807283+hychan0517@users.noreply.github.com>
2019-11-04 20:00:14 +0900

    FireArrow Setting

---
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/FireArrow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using GlobalDefine;
+
+public class FireArrow : BulletPlayerSkill
+{
+	private eAttackType attackType;
+	private float damage;
+	private float speed;
+	private Vector3 dir;
+	public void Setting(eAttackType type,float _damage,float _speed,Vector3 _dir)
+	{
+		attackType = type;
+		damage = _damage;
+		speed = _speed;
+		dir = _dir;
+	}
+	private void Update()
+	{
+		gameObject.transform.position += dir * speed * Time.deltaTime;
+	}
+	public override void Crash(Monster monster)
+	{
+		monster.Damage(attackType, GameMng.Ins.player.calStat.damage, damage);
+		gameObject.SetActive(false);
+		//TODO : 피격이펙트
+	}
+}
--- MiniProject/Assets/01.Script/Player/Skill/Parent/SkillSpawnFireArrow.cs
+++ MiniProject/Assets/01.Script/Player/Skill/Parent/SkillSpawnFireArrow.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.Collections;
 public class SkillSpawnFireArrow : Skill
 {
-
+	public ParticleSystem gateParticle;
+	public FireArrow fireArrow;
 	#region SkillSetting
 	enum eSpawnFireArrowOption
 	{
 		Damage,
 		CoolTime,
 		ActiveTime,
+		ArrowInitTime,
+		ArrowSpeed,
 	}
 	private float damage;
 	private float activeTime;
+	private float arrowInitTime;
+	private float arrowSpeed;
 	public override void SkillSetting()
 	{
 		skillID = 7;
@@ -23,6 +26,8 @@ public class SkillSpawnFireArrow : Skill
 		damage = skillData.optionArr[(int)eSpawnFireArrowOption.Damage];
 		cooldownTime = skillData.optionArr[(int)eSpawnFireArrowOption.CoolTime];
 		activeTime = skillData.optionArr[(int)eSpawnFireArrowOption.ActiveTime];
+		arrowInitTime = skillData.optionArr[(int)eSpawnFireArrowOption.ActiveTime];
+		arrowSpeed = skillData.optionArr[(int)eSpawnFireArrowOption.ActiveTime];
 		delayTime = cooldownTime;
 		gameObject.SetActive(false);
 	}
@@ -40,10 +45,34 @@ public class SkillSpawnFireArrow : Skill
 	{
 		base.OnDrop();
 		ActiveSkill();
+		CreateGate();
+		StartCoroutine(CreateArrow());
 	}
 	private void Update()
 	{
 		delayTime += Time.deltaTime;
+	}
+	private IEnumerator CreateArrow()
+	{
+		float cTime = 0;
+		while(delayTime <= activeTime)
+		{
+			cTime += Time.deltaTime;
+			if(cTime >= arrowInitTime)
+			{
+				cTime -= arrowInitTime;
+				GameObject o = Instantiate(fireArrow.gameObject);
+				yield return null;
+			}
+		}
+	}
+	private void CreateGate()
+	{
+		gateParticle.gameObject.SetActive(true);
+		gateParticle.gameObject.transform.position = GameMng.Ins.player.transform.position;
+		gateParticle.gameObject.transform.eulerAngles = new Vector3(0, 0, GameMng.Ins.player.degree);
+		gateParticle.Play();
+		fireArrow.Setting(skillType, damage, arrowSpeed, gateParticle.gameObject.transform.right);
 	}
 }
 
