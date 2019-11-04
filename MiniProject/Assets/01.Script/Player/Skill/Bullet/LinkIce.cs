3791a8393380d684248a4adf0523ef397cd69f11
성민 한 <kbg01011@gmail.com>
2019-11-05 01:12:41 +0900

    LinkIceBalt

---
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/LinkIce.cs
@@ -0,0 +1,69 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkIce : BulletPlayerSkill
+{
+    public float damage = 0;
+    eAttackType Attacktype = eAttackType.Water;
+    eBuffType bufftype = eBuffType.MoveSlow;
+
+    private int Id = 10;
+    private float per;
+
+    private Vector3 nextPos;
+
+    private int idxnum = 0;
+
+    private List<Monster> monsterpool;
+
+    public void Setting(int id, float p, float damage)
+    {
+        Id = id;
+        per = p;
+        gameObject.transform.position = GameMng.Ins.player.transform.position +
+            new Vector3(0, GameMng.Ins.player.calStat.size);
+        monsterpool = GameMng.Ins.monsterPool.monsterList;
+        NextPosSet();
+        idxnum = 0;
+    }
+
+    private void NextPosSet()
+    {
+        nextPos = GameMng.Ins.player.transform.right;
+
+        for (int i = idxnum; i < monsterpool.Count; ++i)
+        {
+            if (monsterpool[i] == null)
+                continue;
+            nextPos = (monsterpool[idxnum].transform.position +
+                        new Vector3(0, monsterpool[idxnum].monsterData.size) -
+                        gameObject.transform.position);
+            break;
+        }
+        
+        float degree = Mathf.Atan2(nextPos.y, nextPos.x) * Mathf.Rad2Deg;
+        gameObject.transform.eulerAngles = new Vector3(0, 0, degree);
+
+        ++idxnum;
+    }
+
+    private void Update()
+    {
+        if (idxnum >= monsterpool.Count)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        gameObject.transform.position += gameObject.transform.right * Time.deltaTime * 10;
+
+    }
+
+    public override void Crash(Monster monster)
+    {
+        NextPosSet();
+        monster.Damage(Attacktype, damage);
+        monster.OutStateAdd(new ConditionData(bufftype, Id, 10.0f, 500), 1000);
+    }
+}
---
+++ MiniProject/Assets/01.Script/Player/Skill/Parent/SkillLinkIceBalt.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillLinkIceBalt : Skill
+{
+    #region SkillSetting
+    enum eSkillOption
+    {
+        Damage,
+        CoolTime,
+    }
+    private float damage;
+    public override void SkillSetting()
+    {
+        skillID = 10;
+        PlayerSkillData skillData = JsonMng.Ins.playerSkillDataTable[skillID];
+        skillType = skillData.skillType;
+        target = skillData.target;
+        damage = skillData.optionArr[(int)eSkillOption.Damage];
+        cooldownTime = skillData.optionArr[(int)eSkillOption.CoolTime];
+        delayTime = cooldownTime;
+        gameObject.SetActive(false);
+    }
+    #endregion
+    public LinkIce ice;
+
+    //실제 쿨타임 도는 타이밍에 ActiveSkill();
+    public override void OnButtonDown()
+    {
+        base.OnButtonDown();
+        ActiveSkill();
+    }
+    public override void ActiveSkill()
+    {
+        ice.gameObject.SetActive(true);
+        ice.Setting(skillID, 1000, damage);
+        base.ActiveSkill();
+    }
+    public override void OnDrag()
+    {
+        base.OnDrag();
+    }
+    public override void OnDrop()
+    {
+        base.OnDrop();
+    }
+    private void Update()
+    {
+        delayTime += Time.deltaTime;
+        if (delayTime >= 5.0f)
+            gameObject.SetActive(false);
+    }
+}
