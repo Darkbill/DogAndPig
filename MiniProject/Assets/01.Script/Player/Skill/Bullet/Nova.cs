87da1bffec289f9e54f21115819c41a8f5c5e3b6
성민 한 <kbg01011@gmail.com>
2019-11-04 21:14:42 +0900

    IceNova

---
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/Nova.cs
@@ -0,0 +1,54 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nova : BulletPlayerSkill
+{
+    public float damage = 0;
+    eAttackType Attacktype = eAttackType.Water;
+    eBuffType bufftype = eBuffType.MoveSlow;
+
+    private int Id = 8;
+    private float per;
+
+    private int effectCnt = 0;
+
+    public List<ParticleSystem> particlesystem;
+    public CapsuleCollider2D collider2d;
+
+    private Vector2 fixedUpSiz = new Vector2(3.0f / 20, 1.3f / 20);
+
+    public void Setting(int id, float p, float damage)
+    {
+        Id = id;
+        per = p;
+
+        effectCnt = 0;
+        collider2d.size = new Vector2(0, 0);
+    }
+
+    [System.Obsolete]
+    private void FixedUpdate()
+    {
+        if(effectCnt >= 5)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        collider2d.size += fixedUpSiz;
+        if (!particlesystem[1].isPlaying)
+        {
+            for(int i = 0;i<particlesystem.Count;++i)
+                particlesystem[i].Play();
+            collider2d.size = new Vector2(0, 0);
+            ++effectCnt;
+        }
+    }
+
+	public override void Crash(Monster monster)
+	{
+        monster.Damage(Attacktype, damage);
+        monster.OutStateAdd(new ConditionData(bufftype, Id, 10.0f, 500), 1000);
+    }
+}
---
+++ MiniProject/Assets/01.Script/Player/Skill/Parent/SkillIceNova.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillIceNova : Skill
+{
+    #region SkillSetting
+    enum eSkillOption
+    {
+        Damage,
+        CoolTime,
+    }
+    private float damage;
+    public override void SkillSetting()
+    {
+        skillID = 8;
+        PlayerSkillData skillData = JsonMng.Ins.playerSkillDataTable[skillID];
+        skillType = skillData.skillType;
+        target = skillData.target;
+        damage = skillData.optionArr[(int)eSkillOption.Damage];
+        cooldownTime = skillData.optionArr[(int)eSkillOption.CoolTime];
+        delayTime = cooldownTime;
+        gameObject.SetActive(false);
+    }
+    #endregion
+
+    public Nova nova;
+    //실제 쿨타임 도는 타이밍에 ActiveSkill();
+    public override void OnButtonDown()
+    {
+        base.OnButtonDown();
+        ActiveSkill();
+    }
+    public override void ActiveSkill()
+    {
+        nova.Setting(skillID, 1000 /* slow per*/, damage);
+        gameObject.transform.position = GameMng.Ins.player.transform.position +
+            new Vector3(0, GameMng.Ins.player.calStat.size * 3);
+        nova.transform.position = GameMng.Ins.player.transform.position;
+        nova.gameObject.SetActive(true);
+        base.ActiveSkill();
+    }
+    public override void OnDrag()
+    {
+        base.OnDrag();
+    }
+    public override void OnDrop()
+    {
+        base.OnDrop();
+    }
+    private void Update()
+    {
+        delayTime += Time.deltaTime;
+        if (delayTime >= 7.0f)
+            gameObject.SetActive(false);
+        moveset();
+    }
+
+    private void moveset()
+    {
+        gameObject.transform.position = GameMng.Ins.player.transform.position +
+            new Vector3(0, GameMng.Ins.player.calStat.size * 3);
+        nova.transform.position = GameMng.Ins.player.transform.position;
+    }
+}
