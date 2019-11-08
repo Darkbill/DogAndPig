21bbec2b166d40efbb3098b098f204043b103230
crusader0517 <crusader0517@naver.com>
2019-11-08 16:18:57 +0900

    levelDesign 툴

---
+++ MiniProject/Assets/01.Script/StageLevelDesign/DropBoxWorldItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class DropBoxWorldItem : MonoBehaviour
+{
+	public Text worldText;
+	public Toggle toggle;
+	private void Update()
+	{
+		if (toggle.isOn == true)
+		{
+			LevelDesignMng.Ins.ShowStageInfoToWorld(int.Parse(worldText.text));
+		}
+	}
+}
--- MiniProject/Assets/01.Script/StageLevelDesign/LevelDesignMng.cs
+++ MiniProject/Assets/01.Script/StageLevelDesign/LevelDesignMng.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using LitJson;
 using System.IO;
 using UnityEngine.UI;
+using System;
+
 public class LevelDesignMng : MonoBehaviour
 {
 	#region SINGLETON
@@ -26,12 +28,13 @@ public class LevelDesignMng : MonoBehaviour
 		}
 	}
 	#endregion
-	public Dropdown dropDown;
+	public Dropdown stageDropDwon;
+	public Dropdown worldDropDown;
 	public MonsterExam monsterExam;
 	public GameObject gameView;
 	public List<MonsterExam> monsterExamList;
-	public Dictionary<int, List<StageDataTable>> stageDataTable { get; private set; }
-												= new Dictionary<int, List<StageDataTable>>();
+	public Dictionary<int, Dictionary<int, List<StageDataTable>>> stageDataTable { get; private set; }
+												= new Dictionary<int, Dictionary<int, List<StageDataTable>>>();
 	public Text expText;
 	public Text goldText;
 
@@ -39,42 +42,64 @@ public class LevelDesignMng : MonoBehaviour
 	public Text monsterLevel;
 	public Toggle monsterToggle;
 	public Text stageText;
+	public Text worldText;
 	private void Awake()
 	{
 		LoadStageData();
 	}
 	private void Setting()
 	{
-		dropDown.ClearOptions();
-		var i = stageDataTable.GetEnumerator();
+		worldDropDown.ClearOptions();
+		stageDropDwon.ClearOptions();
 		List<int> stage = new List<int>();
-		while(i.MoveNext())
-		{
-			dropDown.options.Add(new Dropdown.OptionData() { text = i.Current.Key.ToString() });
-		}
-		int firstStage = GetFirstStage();
-		ShowGetInfo(firstStage);
-		ShowStageInfo(firstStage);
-		dropDown.value = 0;
-	}
-	private void ShowGetInfo(int stage)
-	{
-		//추가될 수 
 		var i = stageDataTable.GetEnumerator();
-		int count = 0;
 		while (i.MoveNext())
 		{
-			if (i.Current.Key > stage) break;
-			var temp = i.Current.Value;
-			for(int j = 0; j < temp.Count; ++j)
-			{
-				count += temp[j].stageLevel;
-			}
+			worldDropDown.options.Add(new Dropdown.OptionData() { text = i.Current.Key.ToString() });
 		}
-		expText.text = string.Format("EXP : {0}", count.ToString());
-		goldText.text = string.Format("GOLD : {0}", count.ToString());
+		int firstWorld = GetFirstWorld();
+		var j = stageDataTable[firstWorld].GetEnumerator();
+		while(j.MoveNext())
+		{
+			stageDropDwon.options.Add(new Dropdown.OptionData() { text = j.Current.Key.ToString() });
+		}
+		int firstStage = GetFirstStage(firstWorld);
+		ShowGetInfo(firstWorld);
+		ShowStageInfo(firstWorld,firstStage);
+		worldDropDown.value = 0;
+		worldDropDown.captionText.text = firstWorld.ToString();
+		stageDropDwon.value = 0;
+		stageDropDwon.captionText.text = firstStage.ToString();
 	}
-	private int GetFirstStage()
+	private void UpgaradeStageOption(int worldNumber)
+	{
+		stageDropDwon.ClearOptions();
+		var j = stageDataTable[worldNumber].GetEnumerator();
+		while (j.MoveNext())
+		{
+			stageDropDwon.options.Add(new Dropdown.OptionData() { text = j.Current.Key.ToString() });
+		}
+	}
+
+
+	private void ShowGetInfo(int stage)
+	{
+		////추가될 수 
+		//var i = stageDataTable.GetEnumerator();
+		//int count = 0;
+		//while (i.MoveNext())
+		//{
+		//	if (i.Current.Key > stage) break;
+		//	var temp = i.Current.Value;
+		//	for(int j = 0; j < temp.Count; ++j)
+		//	{
+		//		count += temp[j].stageLevel;
+		//	}
+		//}
+		//expText.text = string.Format("EXP : {0}", count.ToString());
+		//goldText.text = string.Format("GOLD : {0}", count.ToString());
+	}
+	private int GetFirstWorld()
 	{
 		var i = stageDataTable.GetEnumerator();
 		int index = int.MaxValue;
@@ -87,10 +112,23 @@ public class LevelDesignMng : MonoBehaviour
 		}
 		return index;
 	}
-	public void ShowStageInfo(int stageNumber)
+	private int GetFirstStage(int WorldNumber)
+	{
+		var i = stageDataTable[WorldNumber].GetEnumerator();
+		int index = int.MaxValue;
+		while (i.MoveNext())
+		{
+			if (i.Current.Key < index)
+			{
+				index = i.Current.Key;
+			}
+		}
+		return index;
+	}
+	public void ShowStageInfo(int worldNumber, int stageNumber)
 	{
 		RemoveMonsterExamList();
-		List<StageDataTable> stageList = stageDataTable[stageNumber];
+		List<StageDataTable> stageList = stageDataTable[worldNumber][stageNumber];
 		for(int i = 0; i < stageList.Count; ++i)
 		{
 			float xPos = stageList[i].enemyPosX;
@@ -104,6 +142,45 @@ public class LevelDesignMng : MonoBehaviour
 		}
 		ShowGetInfo(stageNumber);
 	}
+	public void ShowStageInfoToWorld(int worldNumber)
+	{
+		RemoveMonsterExamList();
+		List<StageDataTable> stageList = stageDataTable[worldNumber][GetFirstStage(worldNumber)];
+		for (int i = 0; i < stageList.Count; ++i)
+		{
+			float xPos = stageList[i].enemyPosX;
+			float yPos = stageList[i].enemyPosY;
+			int monsterIndex = stageList[i].enemyIndex;
+			int monsterLevel = stageList[i].enemyLevel;
+			GameObject o = Instantiate(monsterExam.gameObject, gameView.transform);
+			MonsterExam e = o.GetComponent<MonsterExam>();
+			e.Setting(monsterLevel.ToString(), monsterIndex.ToString(), xPos, yPos, stageList[i].boss);
+			monsterExamList.Add(e);
+		}
+		UpgaradeStageOption(worldNumber);
+		stageDropDwon.value = 0;
+		int firstStage = GetFirstStage(worldNumber);
+		stageDropDwon.captionText.text = firstStage.ToString();
+	}
+	public void ShowStageInfo(int stageNumber)
+	{
+		RemoveMonsterExamList();
+		List<StageDataTable> stageList = stageDataTable[int.Parse(worldDropDown.captionText.text)][stageNumber];
+		for (int i = 0; i < stageList.Count; ++i)
+		{
+			float xPos = stageList[i].enemyPosX;
+			float yPos = stageList[i].enemyPosY;
+			int monsterIndex = stageList[i].enemyIndex;
+			int monsterLevel = stageList[i].enemyLevel;
+			GameObject o = Instantiate(monsterExam.gameObject, gameView.transform);
+			MonsterExam e = o.GetComponent<MonsterExam>();
+			e.Setting(monsterLevel.ToString(), monsterIndex.ToString(), xPos, yPos, stageList[i].boss);
+			monsterExamList.Add(e);
+		}
+		ShowGetInfo(stageNumber);
+		UpgaradeStageOption(int.Parse(worldDropDown.captionText.text));
+		stageDropDwon.captionText.text = stageNumber.ToString();
+	}
 	public void SaveStage()
 	{
 		List<StageDataTable> tempList = new List<StageDataTable>();
@@ -111,16 +188,18 @@ public class LevelDesignMng : MonoBehaviour
 		{
 			if (monsterExamList[i] == null) continue;
 			StageDataTable temp = new StageDataTable();
-			temp.stageLevel = int.Parse(dropDown.captionText.text);
+			temp.worldLevel = int.Parse(worldDropDown.captionText.text);
+			temp.stageLevel = int.Parse(stageDropDwon.captionText.text);
 			temp.enemyIndex = int.Parse(monsterExamList[i].indexText.text);
 			temp.enemyLevel = int.Parse(monsterExamList[i].levelText.text);
+			temp.boss = monsterExamList[i].boss;
 			Vector2 pos = monsterExamList[i].GetCoord();
 			temp.enemyPosX = pos.x;
 			temp.enemyPosY = pos.y;
 			tempList.Add(temp);
 		}
-		stageDataTable[int.Parse(dropDown.captionText.text)] = tempList;
-		ShowGetInfo(int.Parse(dropDown.captionText.text));
+		stageDataTable[int.Parse(worldDropDown.captionText.text)][int.Parse(stageDropDwon.captionText.text)] = tempList;
+		ShowGetInfo(int.Parse(stageDropDwon.captionText.text));
 	}
 	private void RemoveMonsterExamList()
 	{
@@ -135,7 +214,7 @@ public class LevelDesignMng : MonoBehaviour
 	{
 		StartCoroutine(StartLoad("StageDataTable", stageDataTable));
 	}
-	private IEnumerator StartLoad(string fileName, Dictionary<int, List<StageDataTable>> table)
+	private IEnumerator StartLoad(string fileName, Dictionary<int, Dictionary<int,List<StageDataTable>>> table)
 	{
 		string path = string.Format("{0}/LitJson/{1}.json", Application.streamingAssetsPath, fileName);
 		WWW www = new WWW(path);
@@ -144,16 +223,27 @@ public class LevelDesignMng : MonoBehaviour
 		JsonData jsonData = JsonMapper.ToObject(jsonString);
 		for (int i = 0; i < jsonData.Count; ++i)
 		{
-			var save = JsonMapper.ToObject<StageDataTable>(jsonData[i].ToJson());
-			if(table.ContainsKey(save.stageLevel))
+			StageDataTable save = JsonMapper.ToObject<StageDataTable>(jsonData[i].ToJson());
+			if (table.ContainsKey(save.GetTableID()))
 			{
-				table[save.stageLevel].Add(save);
+				if (table[save.GetTableID()].ContainsKey(save.GetStageID()))
+				{
+					table[save.GetTableID()][save.GetStageID()].Add(save);
+				}
+				else
+				{
+					List<StageDataTable> temp = new List<StageDataTable>();
+					temp.Add(save);
+					table[save.GetTableID()].Add(save.GetStageID(), temp);
+				}
 			}
 			else
 			{
+				Dictionary<int, List<StageDataTable>> tempDic = new Dictionary<int, List<StageDataTable>>();
 				List<StageDataTable> temp = new List<StageDataTable>();
 				temp.Add(save);
-				table.Add(save.stageLevel, temp);
+				tempDic.Add(save.GetStageID(), temp);
+				table.Add(save.GetTableID(), tempDic);
 			}
 		}
 
@@ -162,18 +252,24 @@ public class LevelDesignMng : MonoBehaviour
 	public void SaveAll()
 	{
 		string path = string.Format("{0}/LitJson/{1}.json", Application.streamingAssetsPath, "StageDataTable");
-		var i = stageDataTable.GetEnumerator();
+
 		List<StageDataTable> temp = new List<StageDataTable>();
-		while(i.MoveNext())
+		var i = stageDataTable.GetEnumerator();
+		while (i.MoveNext())
 		{
-			if(i.Current.Value.Count == 0)
+			var j = i.Current.Value.GetEnumerator();
+			while(j.MoveNext())
 			{
-				Debug.LogError("비어있는 스테이지가 있습니다.");
-				return;
-			}
-			for(int j = 0; j < i.Current.Value.Count; ++j)
-			{
-				temp.Add(i.Current.Value[j]);
+				if (j.Current.Value.Count == 0)
+				{
+					Debug.LogError("비어있는 스테이지가 있습니다.");
+					return;
+				}
+				for (int k = 0; k < j.Current.Value.Count; ++k)
+				{
+					temp.Add(j.Current.Value[k]);
+				}
+				
 			}
 		}
 		JsonData data = JsonMapper.ToJson(temp);
@@ -181,18 +277,25 @@ public class LevelDesignMng : MonoBehaviour
 	}
 	public void RemoveStage()
 	{
-		stageDataTable.Remove(int.Parse(dropDown.captionText.text));
+		stageDataTable[int.Parse(worldDropDown.captionText.text)].Remove(int.Parse(stageDropDwon.captionText.text));
+		Sort();
+		Setting();
+	}
+	public void RemoveWorld()
+	{
+		stageDataTable.Remove(int.Parse(worldDropDown.captionText.text));
+		Sort();
 		Setting();
 	}
 	public void CreateStage()
 	{
-		if(stageDataTable.ContainsKey(int.Parse(stageText.text)))
+		if(stageDataTable[int.Parse(worldText.text)].ContainsKey(int.Parse(stageText.text)))
 		{
 			Debug.LogError("이미 존재하는 Stage");
 		}
 		else
 		{
-			stageDataTable.Add(int.Parse(stageText.text), new List<StageDataTable>());
+			stageDataTable[int.Parse(worldText.text)].Add(int.Parse(stageText.text), new List<StageDataTable>());
 			Sort();
 			Setting();
 		}
@@ -215,7 +318,8 @@ public class LevelDesignMng : MonoBehaviour
 	public void Sort()
 	{
 		//Stage 별로 다시 저장
-		Dictionary<int, List<StageDataTable>> temp = new Dictionary<int, List<StageDataTable>>();
+		Dictionary<int, Dictionary<int, List<StageDataTable>>> temp = new Dictionary<int, Dictionary<int, List<StageDataTable>>>();
+		
 		while (stageDataTable.Count != 0)
 		{
 			int cIndex = int.MaxValue;
@@ -231,5 +335,28 @@ public class LevelDesignMng : MonoBehaviour
 			stageDataTable.Remove(cIndex);
 		}
 		stageDataTable = temp;
+		var i = stageDataTable.GetEnumerator();
+		Dictionary<int, Dictionary<int, List<StageDataTable>>> tempAll = new Dictionary<int, Dictionary<int, List<StageDataTable>>>();
+		while (i.MoveNext())
+		{
+			var sort = stageDataTable[i.Current.Key];
+			Dictionary<int, List<StageDataTable>> tempValue = new Dictionary<int, List<StageDataTable>>();
+			while (sort.Count != 0)
+			{
+				int cIndex = int.MaxValue;
+				var e = i.Current.Value.GetEnumerator();
+				while (e.MoveNext())
+				{
+					if (cIndex > e.Current.Key)
+					{
+						cIndex = e.Current.Key;
+					}
+				}
+				tempValue.Add(cIndex, sort[cIndex]);
+				sort.Remove(cIndex);
+			}
+			tempAll.Add(i.Current.Key,tempValue);
+		}
+		stageDataTable = tempAll;
 	}
 }
