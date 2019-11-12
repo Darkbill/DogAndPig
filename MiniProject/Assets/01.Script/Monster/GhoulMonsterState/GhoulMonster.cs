565af9a5a8837597423af88d21b84e07558f4287
crusader0517 <crusader0517@naver.com>
2019-11-12 17:36:28 +0900

    애니메이션 호출함수 위치 변경

--- MiniProject/Assets/01.Script/Monster/GhoulMonsterState/GhoulMonster.cs
+++ MiniProject/Assets/01.Script/Monster/GhoulMonsterState/GhoulMonster.cs
@@ -1,5 +1,18 @@
-﻿
+﻿using GlobalDefine;
+using UnityEngine;
 public class GhoulMonster : Monster
 {
-
+	public void AttackCheckEnd()
+	{
+		//애니메이션 호출 함수
+		Vector3 directionToPlayer = GameMng.Ins.player.transform.position - gameObject.transform.position;
+		if (att.BaseAttack(GetForward(), directionToPlayer, monsterData.attackRange, monsterData.attackAngle))
+		{
+			GameMng.Ins.DamageToPlayer(eAttackType.Physics, monsterData.damage);
+			GameMng.Ins.HitToEffect(eAttackType.Physics,
+				GameMng.Ins.player.transform.position + new Vector3(0, GameMng.Ins.player.calStat.size, 0),
+				transform.position + new Vector3(0, monsterData.size, 0),
+				GameMng.Ins.player.calStat.size);
+		}
+	}
 }
--- MiniProject/Assets/01.Script/Monster/Monster.cs
+++ MiniProject/Assets/01.Script/Monster/Monster.cs
@@ -63,19 +63,6 @@ public class Monster : MonoBehaviour
 		}
 		return false;
 	}
-	public void AttackCheckEnd()
-	{
-		//애니메이션 호출 함수
-		Vector3 directionToPlayer = GameMng.Ins.player.transform.position - gameObject.transform.position;
-		if (att.BaseAttack(GetForward(), directionToPlayer, monsterData.attackRange, monsterData.attackAngle))
-		{
-			GameMng.Ins.DamageToPlayer(eAttackType.Physics, monsterData.damage);
-			GameMng.Ins.HitToEffect(eAttackType.Physics,
-				GameMng.Ins.player.transform.position + new Vector3(0, GameMng.Ins.player.calStat.size, 0),
-				transform.position + new Vector3(0, monsterData.size, 0),
-				GameMng.Ins.player.calStat.size);
-		}
-	}
 	#endregion
 
 	#region Buff
