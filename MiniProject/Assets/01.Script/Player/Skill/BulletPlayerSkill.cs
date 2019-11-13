eee3241a830ee4a99aef80efc02efde0c3cf1a99
crusader0517 <crusader0517@naver.com>
2019-11-13 16:49:49 +0900

    세미콜론;;

--- MiniProject/Assets/01.Script/Player/Skill/BulletPlayerSkill.cs
+++ MiniProject/Assets/01.Script/Player/Skill/BulletPlayerSkill.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 public abstract class BulletPlayerSkill : MonoBehaviour
 {
 	public abstract void Crash(Monster monster);
-	public virtual void CrashStay(Monster monster) { };
+	public virtual void CrashStay(Monster monster) { }
 }
