28aff34690742f1f9b1ca716cc5147552720d4ed
성민 한 <kbg01011@gmail.com>
2019-11-17 14:33:21 +0900

    skill16_windstep

---
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/WindKnockback.cs
@@ -0,0 +1,80 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindKnockback : BulletPlayerSkill
+{
+    public ParticleSystem knockbackParticle;
+    //처음 1회만 넉벡적용을 위해 collider 없앰
+    public ParticleSystem bufParticle;
+
+    private eAttackType type = eAttackType.Wind;
+    private eBuffType debuftype = eBuffType.NockBack;
+    private eBuffType bufType = eBuffType.MoveFast;
+
+    private int skillID;
+    private float bufTime;
+    private float setTime = 0f;
+    private float radius;
+    private float speedPer;
+
+    public void Setting(int _skillID, float _buffTime, float _radius, float _speedper)
+    {
+        skillID = _skillID;
+        bufTime = _buffTime;
+        radius = _radius;
+        speedPer = _speedper;
+        gameObject.transform.parent = GameMng.Ins.skillMng.transform;
+        gameObject.SetActive(false);
+    }
+
+    public void SystemSetting()
+    {
+        gameObject.SetActive(true);
+        knockbackParticle.Play();
+        bufParticle.Play();
+        setTime = 0.0f;
+        CrashKnockback();
+        FastBuff();
+    }
+
+    public void OffSkill()
+    {
+        knockbackParticle.Stop();
+        bufParticle.Stop();
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = GameMng.Ins.player.transform.position;
+        setTime += Time.deltaTime;
+        if (setTime > bufTime)
+            gameObject.SetActive(false);
+    }
+
+
+    private void CrashKnockback()
+    {
+        var hitMonsterList = GameMng.Ins.monsterPool.monsterList;
+        foreach(Monster m in hitMonsterList)
+        {
+            if((m.transform.position - GameMng.Ins.player.transform.position).magnitude < radius && m.gameObject.activeSelf)
+                m.OutStateAdd(new ConditionData(debuftype, skillID, 0, radius), 
+                    m.transform.position - GameMng.Ins.player.transform.position);
+        }
+    }
+
+    private void FastBuff()
+    {
+        GameMng.Ins.player.AddBuff(new ConditionData(bufType, skillID, bufTime, speedPer));
+    }
+
+    public override void Crash(Monster monster)
+    {
+        //if (hitMonsterList.Contains(monster)) return;
+        //hitMonsterList.Add(monster);
+        //monster.OutStateAdd(new ConditionData(eBuffType.NockBack, skillID, 0, knockBackPower), monster.transform.position - transform.position);
+    }
+}
---
+++ MiniProject/Assets/01.Script/Player/Skill/Parent/SkillWindStep.cs
@@ -0,0 +1,72 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillWindStep : Skill
+{
+    #region SkillSetting
+    enum eWindStepOption
+    {
+        BufTime,
+        CoolTime,
+        SpeedPer,
+        Radius,
+    }
+    private float buftime;
+    private float speedPer;
+    private float radius;
+
+    public override void SkillSetting()
+    {
+        skillID = 16;
+        PlayerSkillData skillData = JsonMng.Ins.playerSkillDataTable[skillID];
+        buftime = skillData.optionArr[(int)eWindStepOption.BufTime];
+        cooldownTime = skillData.optionArr[(int)eWindStepOption.CoolTime];
+        speedPer = skillData.optionArr[(int)eWindStepOption.SpeedPer];
+        radius = skillData.optionArr[(int)eWindStepOption.Radius];
+        delayTime = cooldownTime;
+    }
+    public override void SetItemBuff(eSkillOption type, float changeValue)
+    {
+        //switch (type)
+        //{
+        //    case eSkillOption.Damage:
+        //        damage += damage * changeValue;
+        //        break;
+        //    case eSkillOption.CoolTime:
+        //        cooldownTime -= cooldownTime * changeValue;
+        //        break;
+        //    case eSkillOption.BuffActivePer:
+        //        activePer += activePer * changeValue;
+        //        break;
+        //}
+    }
+    public override void SetBullet()
+    {
+        windbullet.Setting(skillID, buftime, radius, speedPer);
+    }
+    public override void OffSkill()
+    {
+        windbullet.OffSkill();
+    }
+    #endregion
+
+    public WindKnockback windbullet;
+
+    public override void OnButtonDown()
+    {
+        base.OnButtonDown();
+        ActiveSkill();
+    }
+    public override void ActiveSkill()
+    {
+        base.ActiveSkill();
+        windbullet.SystemSetting();
+    }
+
+    void Update()
+    {
+        delayTime += Time.deltaTime;
+    }
+}
