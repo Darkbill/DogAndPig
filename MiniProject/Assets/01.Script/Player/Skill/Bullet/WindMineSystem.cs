928b8103ff7a74bad47639855d4765d5081afa26
성민 한 <kbg01011@gmail.com>
2019-11-17 22:46:36 +0900

    skill18 windmine

---
+++ MiniProject/Assets/01.Script/Player/Skill/Bullet/WindMineSystem.cs
@@ -0,0 +1,107 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMineSystem : BulletPlayerSkill
+{
+    eAttackType attackType = eAttackType.Wind;
+    eBuffType bufType = eBuffType.Stun;
+
+    private float damage;
+    private float bombDamage;
+    private float radius;
+
+    private const float burningTime = 2.0f;
+    private float setTime = 0.0f;
+
+    public CircleCollider2D minecollider2d;
+    public GameObject minePreparations;
+    public List<GameObject> minePreparationsParticleList = new List<GameObject>();
+    public GameObject windMineBomb;
+    public List<GameObject> windMineBombParticleList = new List<GameObject>();
+
+    public void Setting(float _damage, float _bombDamage, float _radius)
+    {
+        damage = _damage;
+        radius = _radius;
+        bombDamage = _bombDamage;
+        gameObject.transform.parent = GameMng.Ins.skillMng.transform;
+        minePreparations.SetActive(false);
+        windMineBomb.SetActive(false);
+
+        foreach (GameObject p in minePreparationsParticleList)
+            p.gameObject.transform.localScale *= _radius;
+
+        foreach (GameObject p in windMineBombParticleList)
+            p.gameObject.transform.localScale *= _radius;
+    }
+
+    public void SystemSetting(Vector3 _pos)
+    {
+        transform.position = _pos;
+        minePreparations.transform.position = _pos;
+        windMineBomb.transform.position = _pos + new Vector3(0, 0, -10f);
+
+        setTime = 0.0f;
+
+        minecollider2d.enabled = true;
+        gameObject.SetActive(true);
+    }
+
+    public void EndSystemSetting()
+    {
+        minePreparations.SetActive(false);
+        windMineBomb.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!minePreparations.GetComponent<ParticleSystem>().isPlaying && minePreparations.activeSelf)
+        {
+            EndSystemSetting();
+        }
+        if (minePreparations.activeSelf)
+        {
+            setTime += Time.deltaTime;
+            if (setTime > burningTime)
+                MineBomb();
+        }
+    }
+
+    private void MineBomb()
+    {
+        windMineBomb.SetActive(true);
+
+        var monsterpool = GameMng.Ins.monsterPool.monsterList;
+
+        foreach(Monster m in monsterpool)
+        {
+            if (m == null || !m.gameObject.activeSelf) continue;
+            if((m.transform.position - transform.position).magnitude < radius)
+            {
+                m.Damage(attackType, GameMng.Ins.player.calStat.damage, damage);
+                GameMng.Ins.HitToEffect(attackType,
+                    m.transform.position + new Vector3(0, m.monsterData.size),
+                    gameObject.transform.position + new Vector3(-0.3f, -0.15f),
+                    m.monsterData.size);
+            }
+        }
+        setTime = -1f;
+    }
+
+    public override void Crash(Monster monster)
+    {
+        minecollider2d.enabled = false;
+        minePreparations.SetActive(true);
+        minePreparations.GetComponent<ParticleSystem>().Play();
+
+        monster.Damage(attackType, GameMng.Ins.player.calStat.damage, damage);
+        GameMng.Ins.HitToEffect(attackType,
+            monster.transform.position + new Vector3(0, monster.monsterData.size),
+            gameObject.transform.position + new Vector3(-0.3f, -0.15f),
+            monster.monsterData.size);
+    }
+
+}
---
+++ MiniProject/Assets/01.Script/Player/Skill/Parent/SkillWindMine.cs
@@ -0,0 +1,91 @@
+﻿using GlobalDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillWindMine : Skill
+{
+    #region SkillSetting
+    enum eWindMineOption
+    {
+        Damage,
+        CoolTime,
+        BombDamage,
+        Radius,
+    }
+    //TODO : randrange는 해당 값의 /10하고 나오는 값.
+    private float damage;
+    private float bombDamage;
+    private float radius;
+    public override void SkillSetting()
+    {
+        skillID = 18;
+        PlayerSkillData skillData = JsonMng.Ins.playerSkillDataTable[skillID];
+        damage = skillData.optionArr[(int)eWindMineOption.Damage];
+        cooldownTime = skillData.optionArr[(int)eWindMineOption.CoolTime];
+        bombDamage = skillData.optionArr[(int)eWindMineOption.BombDamage];
+        radius = skillData.optionArr[(int)eWindMineOption.Radius];
+        delayTime = cooldownTime;
+        gameObject.SetActive(false);
+    }
+    public override void SetItemBuff(eSkillOption type, float changeValue)
+    {
+        switch (type)
+        {
+            case eSkillOption.Damage:
+                damage += damage * changeValue;
+                break;
+            case eSkillOption.CoolTime:
+                cooldownTime -= cooldownTime * changeValue;
+                break;
+        }
+    }
+    public override void SetBullet()
+    {
+        foreach (WindMineSystem o in mine)
+            o.Setting(damage, bombDamage, radius);
+    }
+    public override void OffSkill()
+    {
+        foreach (WindMineSystem o in mine)
+            o.EndSystemSetting();
+    }
+    #endregion
+
+    public List<WindMineSystem> mine = new List<WindMineSystem>();
+
+    public override void OnButtonDown()
+    {
+        GameMng.Ins.SetSkillAim(skillID);
+    }
+    public override void OnDrop()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        CreateAndPool(mousePos);
+        ActiveSkill();
+    }
+    public override void OnDrop(Vector2 pos)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(pos);
+        CreateAndPool(mousePos);
+        ActiveSkill();
+    }
+    private void CreateAndPool(Vector3 pos)
+    {
+        foreach(WindMineSystem m in mine)
+        {
+            if (m.gameObject.activeSelf) continue;
+            m.SystemSetting(pos);
+            return;
+        }
+        WindMineSystem o = Instantiate(mine[0], GameMng.Ins.skillMng.transform);
+        o.Setting(damage, bombDamage, radius);
+        o.SystemSetting(pos);
+        mine.Add(o);
+    }
+    private void Update()
+    {
+        delayTime += Time.deltaTime;
+    }
+}
