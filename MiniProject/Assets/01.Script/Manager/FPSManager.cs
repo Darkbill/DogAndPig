f3377e9a209993b8c2db4a6a74c8451424859e70
hychan0517 <gjdbscks89@naver.com>
2020-03-02 09:07:04 +0900

    Frame표기

---
+++ MiniProject/Assets/01.Script/Manager/FPSManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSManager : MonoBehaviour
+{
+	public Text frameText;
+	public float worstFrame;
+	public float averagerFrame;
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	private void Update()
+	{
+		float frameTime = Time.deltaTime;
+		string frameTimeStr = string.Format("{0:F2} ms", frameTime * 1000);
+		float frame = 1 / frameTime;
+		string frameStr = string.Format("{0:F2} fps", frame);
+		frameText.text = string.Format("{0} {1}", frameTimeStr, frameStr);
+	}
+}
